2a00b823c3eda2f22b9c7a202214857b16d16ecd
irenebell <irinaselen1@gmail.com>
2022-04-19 12:00:58 +0300

    added file index.html

--- js_hw_1.js
+++ js_hw_1.js
@@ -23,19 +23,94 @@ let item_7 = String(item_6);
 let item_7_type;
 item_7_type = typeof(item_7);
 console.log(`item_7 = ${item_7}, item_7_type = ${item_7_type}`);
-let age_1 = 10;
-let age_2 = 18;
-let age_3 = 60;
-if (age_1 < age_2) {
-  console.log(`You don't have access cause your age is ${age_1}. It's less then 18`);
-} else if (age_1 >= age_2 && age_1 < age_3) {
-  console.log("Welcome !");
-} else if (age_1 >= age_3) {
-  console.log("Keep calm and look Culture channel");
-} else {
-  console.log("Technical work");
-};
 
+//1****************************************************************
 
+const checkAge1 = function(age) {
+  let age_2 = 18;
+  let age_3 = 60;
+    if (age < age_2) {
+    console.log(`You don't have access cause your age is ${age}. It's less then 18`);
+  } else if (age >= age_2 && age < age_3) {
+    console.log("Welcome !");
+  } else if (age >= age_3) {
+    console.log("Keep calm and look Culture channel");
+  } 
+}
 
+checkAge1(17);
+checkAge1(18);
+checkAge1(61);
 
+//2***********************************************************
+
+const checkAge2 = function(age) {
+  let age_2 = 18;
+  let age_3 = 60;
+  if (typeof(age) != "number") {
+   console.log("Error");
+  } else {
+  if (age < age_2) {
+    console.log(`You don't have access cause your age is ${age}. It's less then 18`);
+  } else if (age >= age_2 && age < age_3) {
+    console.log("Welcome !");
+  } else if (age >= age_3) {
+    console.log("Keep calm and look Culture channel");
+  } 
+}
+}
+
+checkAge2(17);
+checkAge2(18);
+checkAge2(61);
+checkAge2("cvv");
+checkAge2("2a"); 
+
+//3**********************************************************
+
+const checkAge3 = function(age) {
+  let age_2 = 18;
+  let age_3 = 60;
+  age = Number(age)
+  if (isNaN(age)) {
+   console.log("Error");
+  } else {
+    if (age < age_2) {
+    console.log(`You don't have access cause your age is ${age}. It's less then ${age_2}`);
+  } else if (age >= age_2 && age < age_3) {
+    console.log("Welcome !");
+  } else if (age >= age_3) {
+    console.log("Keep calm and look Culture channel");
+  } 
+}
+}
+
+checkAge3(17);
+checkAge3(18);
+checkAge3(61);
+checkAge3("cvv");
+checkAge3("2a");
+checkAge3("2");
+
+//4***********************************************************
+
+let input = prompt("Enter your age");
+let userAge = Number(input);
+
+const checkAge4 = function(age) {
+  let age_2 = 18;
+  let age_3 = 60;  
+  if (isNaN(age)) {
+   alert("Error");
+  } else {
+  if (age < age_2) {
+    alert(`You don't have access cause your age is ${age}. It's less then ${age_2}`);
+  } else if (age >= age_2 && age < age_3) {
+    alert("Welcome !");
+  } else if (age >= age_3) {
+    alert("Keep calm and look Culture channel");
+  } 
+}
+}
+
+checkAge4(userAge);
